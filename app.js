d7d61b132b7cfb22ef75d06a49e6b33e64c23a4b
mehdi <mehraf@gmail.com>
2024-08-09 17:24:13 +0200

    GES-6 Supprimer des tâches

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ window.onload = function () {
     var name = document.getElementById("name");
     var type = document.getElementById("type");
     var desc = document.getElementById("description");
+    
 
     monForm.onsubmit = function (e) {
 
@@ -18,10 +19,24 @@ window.onload = function () {
             var taskname = newrow.insertCell(0);
             var tasktype = newrow.insertCell(1);
             var taskdesc = newrow.insertCell(2);
+            var taskdelete = newrow.insertCell(3);
 
             taskname.textContent = name.value;
             tasktype.textContent = type.value;
             taskdesc.textContent = desc.value;
+
+            // Création d'un bouton supprimer
+
+            var deleteButton = document.createElement("button"); // Créer un élement html <button>
+            deleteButton.type = "button";
+            deleteButton.textContent = "Supprimer";
+            deleteButton.addEventListener("click", function() {
+                var ligne = this.parentNode.parentNode; // ligne = <tr> qui contient notre bouton
+                ligne.parentNode.removeChild(ligne); // ligne.parentNode = <tbody>
+            });
+
+            // Ajouter le bouton à notre cellule
+            taskdelete.appendChild(deleteButton);
 
             // Rénitialise les champs du formulaire
             monForm.reset();
@@ -32,4 +47,7 @@ window.onload = function () {
         
         }
     }
+
+
+    
 }
